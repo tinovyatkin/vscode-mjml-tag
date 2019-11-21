045d8025fa529898769b6daefc2bda18394edf7c
Konstantin Vyatkin <tino@vtkn.io>
2019-11-20 23:16:39 -0400

    first commit

---
+++ test/example.ts
@@ -0,0 +1,12 @@
+const mjml = (...args: any[]) => args;
+
+export const MJMLHtml = html`
+<mjml>
+  <mj-body>
+    <mj-section full-width="full-width" background-color="red">
+      <!-- your code here -->
+    </mj-section>
+  </mj-body>
+  </mjml>
+`;
+
