a017ec994f58a54925e25e67896d987fb19550f3
Konstantin Vyatkin <tino@vtkn.io>
2019-12-09 13:40:20 -0400

    v1

--- test/example.ts
+++ test/example.ts
@@ -1,12 +1,31 @@
 const mjml = (...args: any[]) => args;
 
-export const MJMLHtml = html`
-<mjml>
-  <mj-body>
-    <mj-section full-width="full-width" background-color="red">
-      <!-- your code here -->
-    </mj-section>
-  </mj-body>
+const abcd = 1;
+const d = { a: "eee" };
+
+export const MJMLHtml = mjml`
+  <mjml>
+    <mj-head>
+      <mj-attributes>
+        <mj-class name="mjclass" color="#234235" font-size="30px" />
+      </mj-attributes>
+      <style>
+        .${abcd} {
+          font-family: "ee";
+        }
+      </style>
+      <mj-style>
+        .ss { font-family: 'ee'; border: 'Lucida Sans', 'Lucida Sans Regular',
+        'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif' };
+        
+        ::root { border-radius: 1px; }
+      </mj-style>
+    </mj-head>
+    <mj-body>
+      <mj-section full-width="${abcd}">
+        <!-- your code here -->
+        <mj-attributes> </mj-attributes>
+      </mj-section>
+    </mj-body>
   </mjml>
 `;
-
